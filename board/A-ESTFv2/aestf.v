9eef0b766b296767102abfac7849c103360cba25
forth32 <you@example.com>
2021-03-02 22:33:43 +0300

    Сделано управление тактовой частотой настройки PLL теперь задаются в конфиге для каждого процессора отдельно

--- board/A-ESTFv2/aestf.v
+++ board/A-ESTFv2/aestf.v
@@ -66,27 +66,14 @@ wire clk_p;
 wire clk_n;
 wire sdclock;
 wire clkrdy;
-wire pll_c0;
-wire pll_c1;
 
-pll100 corepll
-(
+pll pll1 (
    .inclk0(clk50),
-   .c0(pll_c0),     // 100МГц прямая фаза, основная тактовая частота
-   .c1(pll_c1),     // 100МГц инверсная фаза
+   .c0(clk_p),     // 100МГц прямая фаза, основная тактовая частота
+   .c1(clk_n),     // 100МГц инверсная фаза
    .c2(sdclock),     // 12.5 МГц тактовый сигнал SD-карты
    .locked(clkrdy) // флаг готовности PLL
 );
-
-// Если PLL не используется - подключаем синхросигналы к 50МГц
-`ifdef nopll
-assign clk_p=clk50;
-assign clk_n=~clk50;
-`else
-// Иначе подключаем их к PLL
-assign clk_p=pll_c0;
-assign clk_n=pll_c1;
-`endif
 
 //**********************************
 //* Модуль динамической памяти
